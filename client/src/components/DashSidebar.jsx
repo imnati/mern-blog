69e4d1486cc2372cb5121f17e3ccec1f62dfc58b
Natnael Akalewold <natnaelakalewold@gmail.com>
2024-05-31 06:22:24 -0700

    complete user image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -28,6 +28,7 @@ export default function DashSidebar() {
               icon={HiUser}
               label={"User"}
               labelColor="dark"
+              as="div"
             >
               Profile
             </SidebarItem>
