95ae3e3c749b6d79b25cc718f01c6005b71172b6
Natnael Akalewold <natnaelakalewold@gmail.com>
2024-06-17 01:36:04 -0700

    add call to action to the post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,33 @@
+import { Button } from "flowbite-react";
+
+export default function CallToAction() {
+  return (
+    <div
+      className="flex flex-col sm:flex-row p-3 border border-teal-500
+      justify-center items-center rounded-tl-3xl rounded-br-3xl text-center"
+    >
+      <div className="flex-1 justify-center flex flex-col">
+        <h2 className="text-2xl">Want to learn more about JavaScript?</h2>
+        <p className="text-gray-500 my-2">Checkout these resources</p>
+        <Button
+          gradientDuoTone="purpleToBlue"
+          className="rounded-tl-xl rounded-bl-none"
+        >
+          <a
+            href="https://www.freecodecamp.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Freecodecamp
+          </a>
+        </Button>
+      </div>
+      <div className="p-7 flex-1">
+        <img
+          src="https://images.idgesg.net/images/article/2023/04/shutterstock_1361674454-100939444-large.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
