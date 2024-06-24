cbc0c6b51b2c3c85511b9715fd3628737ce7e2f1
Natnael Akalewold <natnaelakalewold@gmail.com>
2024-06-24 02:16:13 -0700

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,33 @@
-import React from "react";
-
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              fugiat mollitia eaque pariatur maiores enim suscipit dolores at
+              consectetur, reiciendis repellendus repudiandae obcaecati facere
+              necessitatibus magnam, esse culpa. Voluptatibus, quas?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              fugiat mollitia eaque pariatur maiores enim suscipit dolores at
+              consectetur, reiciendis repellendus repudiandae obcaecati facere
+              necessitatibus magnam, esse culpa. Voluptatibus, quas?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              fugiat mollitia eaque pariatur maiores enim suscipit dolores at
+              consectetur, reiciendis repellendus repudiandae obcaecati facere
+              necessitatibus magnam, esse culpa. Voluptatibus, quas?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import CallToAction from "../components/CallToAction";
 
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        Currently I don't have any project to show, but to see my progresses and
+        get contacted for any projects, here is my Github account,
+        <span className="text-teal-500 text-lg hover:underline p-2">
+          <a href="https://github.com/imnati" target="_blank">
+            My Github account
+          </a>
+        </span>{" "}
+        and Telegram account
+        <span className="text-teal-500 text-lg hover:underline p-2">
+          <a href="https://t.me/imnati" target="_blank">
+            My Telegram account
+          </a>
+        </span>
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
